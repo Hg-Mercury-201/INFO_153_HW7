527ff2f8ccb9ac03e558bb5872612022378d1da0
Nathan Beebe <njb99@drexel.edu>
2019-06-12 23:26:35 -0400

    removed keys

--- mainTwy.py
+++ mainTwy.py
@@ -1,10 +1,10 @@
 from twython import Twython
 from pymongo import MongoClient
 
-consumer_key = 'p6tClVvRPB6T8QIXosSNd6zkH'
-consumer_secret = 'jNR4VftF0dC7AEVvsePJNOlZOUrERGrQMvopiG8N3TDx95Nbe8'
-access_token = '1097916952880779267-FGv9uJYb6PkU8aebe2tCNjQECxJ7gy'
-access_secret = 'Gb707a8CRLGCZHxfBzFGEvnIQxn5dMq2iKpdrHL4W4Xq7'
+consumer_key = ''
+consumer_secret = ''
+access_token = ''
+access_secret = ''
 dbclient = MongoClient('localhost',27017)
 db = dbclient['twitter_data']
 collection_tweet = db['tweets']
@@ -17,5 +17,31 @@ client = Twython(consumer_key,
 results = client.search(q='tesla',count=100)
 
 collection_tweet.insert_one(results)
+#search tweets "lang = en"
+for x in collection_tweet.find({},{"lang = en":1}):
+    print (x)
+sum = 0
+x = list(collection_tweet.aggregate(
+    [
+        {
+            '$group' :
+            {'_id' : 1,
+            'total_retweet': {'$sum':'$retweet_count'}
+        }
+        }
+    ]))
+print ("retweet total: {}".format(x[0]['total_retweet']))
+
+x = list(collection_tweet.aggregate(
+    [
+        {
+            '$group' :
+            {'_id' : '$user',
+            'count': {'$sum':'1'}
+        }
+        }
+    ]))
+for y in range(0,len(x)):
+    print (x[y])
 dbclient.close()
 print("Done")
